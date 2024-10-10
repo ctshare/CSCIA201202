c786dbc41d9de8248d84940f11cdd8c99d17ad5b
ctshare <168668532+ctshare@users.noreply.github.com>
2024-10-10 14:02:59 -0400

    Update local_update.py

--- content/plugin/local_update.py
+++ content/plugin/local_update.py
@@ -3,11 +3,11 @@ display(HTML("""
 <button type="button" id="button_for_indexeddb">Click This Button Before Typing Anything</button>
 <script>
 window.button_for_indexeddb.onclick = function(e) {
+    // Change the button text to "Local Environment Successfully Updated!" after it is clicked
+    e.target.innerText = "Local Environment Successfully Updated!";
+    
     window.indexedDB.open('JupyterLite Storage').onsuccess = function(e) {
-        // There are also other tables that I'm not clearing:
-        // "counters", "settings", "local-storage-detect-blob-support"
         let tables = ["checkpoints", "files"];
-
         let db = e.target.result;
         let t = db.transaction(tables, "readwrite");
 
