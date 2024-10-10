06640950fd9989666b5f8446b0dfe24cc2be766d
ctshare <168668532+ctshare@users.noreply.github.com>
2024-10-10 13:59:02 -0400

    Create local_update2.py

---
+++ content/plugin/local_update2.py
@@ -0,0 +1,30 @@
+from IPython.display import display, HTML
+display(HTML("""
+<button type="button" id="button_for_indexeddb">Click This Button Before Typing Anything</button>
+<script>
+window.button_for_indexeddb.onclick = function(e) {
+    // Change the button text to "Local Env Successfully Updated!" after it is clicked
+    e.target.innerText = "Local Env Successfully Updated!";
+    
+    window.indexedDB.open('JupyterLite Storage').onsuccess = function(e) {
+        let tables = ["checkpoints", "files"];
+        let db = e.target.result;
+        let t = db.transaction(tables, "readwrite");
+
+        function clearTable(tablename) {
+            let st = t.objectStore(tablename);
+            st.count().onsuccess = function(e) {
+                console.log("Deleting " + e.target.result + " entries from " + tablename + "...");
+                st.clear().onsuccess = function(e) {
+                    console.log(tablename + " is cleared!");
+                }
+            }
+        }
+
+        for (let tablename of tables) {
+            clearTable(tablename);
+        }
+    }
+};
+</script>
+"""))
